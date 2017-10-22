c8e174e9b2001ec927f74ef8d1d61930817d3d7c
MartinPetrovWasTaken <martin033@abv.bg>
2017-10-22 20:09:36 +0300

    Update intervals_controller.rb

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -9,7 +9,14 @@ class IntervalsController < ApplicationController
       csv_file_path = csv_file.path
 
       sum = 0
+      first_row = true
       CSV.foreach(csv_file_path) do |row|
+                
+             if first_row == true
+                first_row = false
+                next
+            end
+                
       if row[2].to_i.odd?
             sum += row[0].to_f
         end
